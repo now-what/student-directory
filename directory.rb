a552d0b86bb15cdc8e390c9593d19067cd5d6a28
Francesco <francesco@monzo.com>
2022-01-17 16:38:42 +0100

    Added user input

--- directory.rb
+++ directory.rb
@@ -29,7 +29,28 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code.
+  # If user hits return twice, we return the array of students 
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 # calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
