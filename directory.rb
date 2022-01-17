ad1dc7e5e8f29f1e4661bc1b001e0528ddf4b164
Francesco <francesco@monzo.com>
2022-01-17 15:55:57 +0100

    Added student count variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # printing the list of students line by line
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # printing the total number of students
-print "Overall, we have "
-# output will be on a single line
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
