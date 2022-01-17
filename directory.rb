27b36d1e3654444d0f02d14332e0d5a1f11bf068
Francesco <francesco@monzo.com>
2022-01-17 16:10:47 +0100

    Grouped actions into methods

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 # printing student list
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
 # printing the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# calling the methods
+print_header
+print(students)
+print_footer(students)
